ca32f23bb38138f770328aca4882cb4f87f49b31
mohsaad <msaad96@gmail.com>
2017-03-12 03:01:50 -0500

    finished plotting, now need to do some stats

--- msa-ece-graph/msa-network.py
+++ msa-ece-graph/msa-network.py
@@ -5,12 +5,18 @@
 import sys
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+
+from random import random
 
 # Generate a graph
 G = nx.Graph()
 
 # people dictionaries
 people = {}
+name_dict = {}
+class_people = [[],[],[],[]]
 
 # read that file
 '''
@@ -28,16 +34,31 @@ with open(sys.argv[1], 'r') as f:
         data_arr = f.readline().split("\n")[0].split(" ")
 
         people[int(data_arr[2])] = (data_arr[0] + " " + data_arr[1], int(data_arr[3]))
+        name_dict[int(data_arr[2])] = data_arr[0] + " " + data_arr[1]
+        class_people[int(data_arr[3])].append(int(data_arr[2]))
         G.add_node(int(data_arr[2]))
 
     num_edges = int(f.readline().split("\n")[0])
     for i in range(0, num_edges):
         edge = f.readline().split("\n")[0].split(" ")
-        print(edge)
         G.add_edge(int(edge[0]), int(edge[1]))
 
 f.close()
 
-plt.figure()
-nx.draw(G)
+
+pos = nx.spring_layout(G)
+
+# plot the figure
+f = plt.figure()
+nx.draw_networkx_nodes(G, pos = pos, nodelist = class_people[0], node_color = 'yellow', label = 'Seniors', labels = name_dict)
+nx.draw_networkx_nodes(G, pos = pos, nodelist = class_people[1], node_color = 'orange', label = 'Juniors', labels = name_dict)
+nx.draw_networkx_nodes(G, pos = pos, nodelist = class_people[2], node_color = 'green', label = 'Sophomores', labels = name_dict)
+nx.draw_networkx_nodes(G, pos = pos, nodelist = class_people[3], node_color = 'red', label = 'Freshman', labels = name_dict)
+nx.draw_networkx_edges(G, pos = pos)
+nx.draw_networkx_labels(G, pos = pos, labels = name_dict, font_color = 'b')
+plt.legend()
+plt.title("Network of Muslim ECE Students")
 plt.show()
+
+# Some statistics
+# rank the most-connected people
